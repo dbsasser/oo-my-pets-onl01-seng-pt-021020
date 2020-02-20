af9d5545e8c6cb5580fac83154bf8045f7732128
Daniel Sasser <danielbsasser@gmail.com>
2020-02-20 01:03:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,11 +2,13 @@ class Owner
   attr_reader :name, :species
   
   @@all = []
+  @@count = 0
   
   def initialize(name)
     @name = name
     @species = "human"
     @@all << self
+    @@count += 1
   end
   
   def say_species
